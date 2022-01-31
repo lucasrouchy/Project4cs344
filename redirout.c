b669c4be9fea40f0827bd9a71b28c7bed64d35bf
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-01-30 21:37:35 -0800

    still need to take care of the parent and the read and write

--- redirout.c
+++ redirout.c
@@ -8,4 +8,17 @@ int main(int argc, char **argv){
 		printf("Error:3 inputs not entered \n");
 		exit(1);
 	}
-int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+int opf = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+int pip[2];
+
+pipe(pip);
+
+if(fork() == 0) {
+  dup2(pip[1],1)
+  close(pip[0]);
+
+  char *const *args = argv + 2;
+  execvp(argv[2], args);
+  exit(1);
+}
