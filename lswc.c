2c11430fb804ce2a38d274270fc63fff7b0fbacc
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-01-30 21:33:53 -0800

    updated the lswc to work withh the pipe function

--- lswc.c
+++ lswc.c
@@ -3,19 +3,19 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	int fds[2];
+	int pip[2];
 
-	pipe(fds);
+	pipe(pip);
 
 	if (fork() == 0) {
-		dup2(fds[0], 0);
-		close(fds[1]);
+		dup2(pip[0], 0);
+		close(pip[1]);
 		execlp("wc", "wc", "-l",NULL);
 		exit(1);
 	}
 
-	dup2(fds[1], 1);
-	close(fds[0]);
+	dup2(pip[1], 1);
+	close(pip[0]);
 
 	execlp("ls", "ls", "-1", "-a", argv[1] ,NULL);
 }
